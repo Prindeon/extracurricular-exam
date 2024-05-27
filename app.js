4e4a4222bfe7298a61a9c8ab1c205c7036a0ed63
Prindeon <marius.k@hotmail.dk>
2024-05-27 11:08:38 +0200

    dfvvdfdsv

--- app.js
+++ app.js
@@ -3,6 +3,48 @@
 window.addEventListener("load", initApp); // When the page is loaded, run initApp function
 
 // Function to initialize the Web App
-function initApp() {
-  console.log("initApp: app.js is running 🎉"); // Log to the console that the app is running
+async function initApp() {
+  const teachers = await getTeachers()
+  console.log(teachers)
+  displayTeachers(teachers)
+  displayTeacherGrid(teachers)
 }
+
+async function getTeachers() {
+  const response = await fetch("https://raw.githubusercontent.com/cederdorff/race/master/data/users.json")
+  const data = await response.json()
+      return data
+}
+
+    const teachersList = document.querySelector("#teachers-list");
+
+function displayTeachers(teachers) {
+    for (const teacher of teachers) {
+    teachersList.insertAdjacentHTML(
+      "beforeend",
+      ` 
+      <li>
+        ${teacher.name} --- ${teacher.mail}
+      </li>
+      `
+    )
+  }  
+}
+
+function displayTeacherGrid(teachers) {
+  const teachersGrid = document.querySelector("#teachers-grid")
+  for (const teacher of teachers) {
+    teachersGrid.insertAdjacentHTML(
+      "beforeend",
+      /*html*/
+      `
+        <article class="grid-item">
+          <img src="${teacher.image}" alt="${teacher.name}" />
+          <h2>${teacher.name}</h2>
+          <p>${teacher.title}</p>
+        </article>
+      `
+      )
+  }
+}
+
